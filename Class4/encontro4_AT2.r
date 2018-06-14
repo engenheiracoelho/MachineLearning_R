d92b82b1b43f99131f435da88c7b09e45b722a8a
Leticia <leticia.ac23@gmail.com>
2018-06-13 21:45:35 -0300

    predict

--- Class4/encontro4_AT2.r
+++ Class4/encontro4_AT2.r
@@ -3,17 +3,17 @@
 ## Example: Predicting Medical Expenses ----
 ## Step 2: Exploring and preparing the data ----
 insurance <- read.csv("insurance.csv", stringsAsFactors = TRUE)
+insurance$age2 <- insurance$age^2
 str(insurance)
 
 # tables
 table(insurance$region)
 table(insurance$sex)
 table(insurance$smoker)
-table(insurance$age)
 table(insurance$expenses) #Type of data
 
 # exploring relationships among features: correlation matrix, correlation between 2 features
-cor(insurance[c("age", "bmi", "children", "expenses","pre")])
+cor(insurance[c("age", "bmi", "children", "expenses")])
 
 # visualing relationships among features: scatterplot matrix
 pairs(insurance[c("age", "bmi", "children", "expenses")])
@@ -23,9 +23,9 @@ library(psych)
 pairs.panels(insurance[c("age", "bmi", "children", "expenses")])
 
 ## Step 3: Training a model on the data ----
-ins_model <- lm(expenses ~ age + children + bmi + sex + smoker + region,
+ins_model <- lm(expenses ~ age2 + children + bmi + sex + smoker + region,
                 data = insurance)
-ins_model <- lm(expenses ~ ., data = insurance) # this is equivalent to above
+#ins_model <- lm(expenses ~ ., data = insurance) # this is equivalent to above
 
 # see the estimated beta coefficients
 ins_model
--- Class4/encontro4_AT3.r
+++ Class4/encontro4_AT3.r
@@ -3,17 +3,20 @@
 ## Example: Predicting Medical Expenses ----
 ## Step 2: Exploring and preparing the data ----
 insurance <- read.csv("insurance.csv", stringsAsFactors = TRUE)
+insurance$age2 <- insurance$age^2
+insurance$sex2 <- insurance$sex^2
+insurance$children2 <- insurance$children^2
+insurance$bmi30 <- ifelse(insurance$bmi >= 30,1,0)
 str(insurance)
 
 # tables
 table(insurance$region)
 table(insurance$sex)
 table(insurance$smoker)
-table(insurance$age)
 table(insurance$expenses) #Type of data
 
 # exploring relationships among features: correlation matrix, correlation between 2 features
-cor(insurance[c("age", "bmi", "children", "expenses","pre")])
+cor(insurance[c("age", "bmi", "children", "expenses")])
 
 # visualing relationships among features: scatterplot matrix
 pairs(insurance[c("age", "bmi", "children", "expenses")])
@@ -23,9 +26,9 @@ library(psych)
 pairs.panels(insurance[c("age", "bmi", "children", "expenses")])
 
 ## Step 3: Training a model on the data ----
-ins_model <- lm(expenses ~ age + children + bmi + sex + smoker + region,
+ins_model <- lm(expenses ~ age2 + age + region + children  + children2 + bmi30 + bmi+ sex*smoker + sex2 + smoker*bmi30 + region,
                 data = insurance)
-ins_model <- lm(expenses ~ ., data = insurance) # this is equivalent to above
+#ins_model <- lm(expenses ~ ., data = insurance) # this is equivalent to above
 
 # see the estimated beta coefficients
 ins_model
